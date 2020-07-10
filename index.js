f27b356c55fa4f38486a915171ecb125a1c5fa8b
Bawan Majid <bawanmajid23@gmail.com>
2020-07-10 08:51:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let multiply = () => {
   return 5*100;
 }
-divide();
+multiply();
 
 let square = (x) => {
   return x* 2;
@@ -16,4 +16,4 @@ add(9,12);
 let subtract = (x,y) => {
   return x - y;
 }
-add(8,3);
+subtract(8,3);
